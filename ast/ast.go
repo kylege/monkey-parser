58928bcc8b8db2fdfc87158d4a19c316a4afaa4c
kyle <kylechen@futunn.com>
2024-01-30 17:32:03 +0800

    解析bool表达式

--- ast/ast.go
+++ ast/ast.go
@@ -168,3 +168,16 @@ func (i *InfixExpression) String() string {
 	b.WriteString(")")
 	return b.String()
 }
+
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode() {}
+func (b *Boolean) TokenLiteral() string {
+	return b.Token.Literal
+}
+func (b *Boolean) String() string {
+	return b.Token.Literal
+}
