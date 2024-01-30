e326d8f0aa7688695ac247c634c9ccf4dbcd0133
kyle <kylechen@futunn.com>
2024-01-30 17:58:10 +0800

    解析if-else表达式

--- ast/ast.go
+++ ast/ast.go
@@ -181,3 +181,45 @@ func (b *Boolean) TokenLiteral() string {
 func (b *Boolean) String() string {
 	return b.Token.Literal
 }
+
+type BlockStatement struct {
+	Token      token.Token
+	Statements []Statement
+}
+
+func (bs *BlockStatement) statementNode() {}
+
+func (bs *BlockStatement) TokenLiteral() string {
+	return bs.Token.Literal
+}
+func (bs *BlockStatement) String() string {
+	var b strings.Builder
+	for _, s := range bs.Statements {
+		b.WriteString(s.String())
+	}
+	return b.String()
+}
+
+type IfExpression struct {
+	Token       token.Token
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) expressionNode() {}
+func (ie *IfExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+func (ie *IfExpression) String() string {
+	var b strings.Builder
+	b.WriteString("if")
+	b.WriteString(ie.Condition.String())
+	b.WriteString(" ")
+	b.WriteString(ie.Consequence.String())
+	if ie.Alternative != nil {
+		b.WriteString("else ")
+		b.WriteString(ie.Alternative.String())
+	}
+	return b.String()
+}
